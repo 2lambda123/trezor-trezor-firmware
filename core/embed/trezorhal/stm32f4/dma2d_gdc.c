7282b34549455fc95f91ed97bbe8cc3515c32293
cepetr <petr@cerninovi.cz>
2024-03-17 13:01:59 +0100

    WIP - drawlib - dma2d optimization test

--- core/embed/trezorhal/stm32f4/dma2d_gdc.c
+++ core/embed/trezorhal/stm32f4/dma2d_gdc.c
@@ -44,6 +44,7 @@ bool dma2d_rgb565_fill(const dma2d_params_t* dp) {
   dma2d_wait();
 
   if (dp->src_alpha == 255) {
+
     dma2d_handle.Init.ColorMode = DMA2D_OUTPUT_RGB565;
     dma2d_handle.Init.Mode = DMA2D_R2M;
     dma2d_handle.Init.OutputOffset =
@@ -53,6 +54,16 @@ bool dma2d_rgb565_fill(const dma2d_params_t* dp) {
     HAL_DMA2D_Start(&dma2d_handle, gdc_color_to_color32(dp->src_fg),
                     (uint32_t)dp->dst_row + dp->dst_x * sizeof(uint16_t),
                     dp->width, dp->height);
+/*
+
+    MODIFY_REG(dma2d_handle.Instance->CR, DMA2D_CR_MODE, DMA2D_R2M);
+    MODIFY_REG(dma2d_handle.Instance->OPFCCR, DMA2D_OPFCCR_CM, DMA2D_OUTPUT_RGB565);
+    MODIFY_REG(dma2d_handle.Instance->OOR, DMA2D_OOR_LO, dp->dst_stride / sizeof(uint16_t) - dp->width);
+    MODIFY_REG(dma2d_handle.Instance->NLR, (DMA2D_NLR_NL|DMA2D_NLR_PL), (dp->height| (dp->width << 16)));
+    WRITE_REG(dma2d_handle.Instance->OMAR, (uint32_t)dp->dst_row + dp->dst_x * sizeof(uint16_t));
+    WRITE_REG(dma2d_handle.Instance->OCOLR, gdc_color_to_color32(dp->src_fg));
+     ((dma2d_handle).Instance->CR |= DMA2D_CR_START);
+*/
   } else {
     // STM32F4 can not accelerate blending with the fixed color
     uint16_t* dst_ptr = (uint16_t*)dp->dst_row + dp->dst_x;
